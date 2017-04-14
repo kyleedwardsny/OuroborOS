c380c684e26a5be01b20b0a9325ad85cb6eaedab
Kyle Edwards <kyleedwardsny@gmail.com>
2017-04-13 20:16:04 -0400

    Add printf format attribute to *printf functions

--- include/ouroboros/stdio.h
+++ include/ouroboros/stdio.h
@@ -6,12 +6,18 @@
 
 #include <ouroboros/unistd.h>
 
-int ou_sprintf(char *str, const char *format, ...);
+#ifdef __GNUC__
+#define GCC_FORMAT_PRINTF(s, f)	__attribute__((format (printf, s, f)))
+#else
+#define GCC_FORMAT_PRINTF(s, f)
+#endif
 
-int ou_vsprintf(char *str, const char *format, va_list args);
+int ou_sprintf(char *str, const char *format, ...) GCC_FORMAT_PRINTF(2, 3);
 
-int _ou_fprintf(ou_ssize_t (*write_cb)(void *cb_data, const void *data, size_t len), void *cb_data, const char *format, ...);
+int ou_vsprintf(char *str, const char *format, va_list args) GCC_FORMAT_PRINTF(2, 0);
 
-int _ou_vfprintf(ou_ssize_t (*write_cb)(void *cb_data, const void *data, size_t len), void *cb_data, const char *format, va_list args);
+int _ou_fprintf(ou_ssize_t (*write_cb)(void *cb_data, const void *data, size_t len), void *cb_data, const char *format, ...) GCC_FORMAT_PRINTF(3, 4);
+
+int _ou_vfprintf(ou_ssize_t (*write_cb)(void *cb_data, const void *data, size_t len), void *cb_data, const char *format, va_list args) GCC_FORMAT_PRINTF(3, 0);
 
 #endif /* OUROBOROS_STDIO_H */
