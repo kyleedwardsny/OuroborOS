7bf89038990e8cd0673ec343f703224fe83128df
Kyle Edwards <kyleedwardsny@gmail.com>
2017-04-16 17:22:05 -0400

    Restructure FDT/FIT build

--- cmake/OuCommon.cmake
+++ cmake/OuCommon.cmake
@@ -1,10 +1,22 @@
+macro(get_source_path VAR FILENAME)
+	if (IS_ABSOLUTE "${FILENAME}")
+		set("${VAR}" "${FILENAME}")
+	else()
+		get_filename_component("${VAR}" "${FILENAME}" ABSOLUTE)
+	endif()
+endmacro()
+
+macro(get_binary_path VAR FILENAME)
+	if (IS_ABSOLUTE "${FILENAME}")
+		set("${VAR}" "${FILENAME}")
+	else()
+		set("${VAR}" "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}")
+	endif()
+endmacro()
+
 macro(add_sources TARGET)
 	foreach (_source IN ITEMS ${ARGN})
-		if (IS_ABSOLUTE "${_source}")
-			set(_source_abs "${_source}")
-		else()
-			get_filename_component(_source_abs "${_source}" ABSOLUTE)
-		endif()
+		get_source_path(_source_abs "${_source}")
 		set_property(GLOBAL APPEND PROPERTY "${TARGET}_SOURCES" "${_source_abs}")
 	endforeach()
 endmacro()
@@ -14,11 +26,7 @@ macro(get_sources VAR TARGET)
 endmacro()
 
 macro(set_link_script TARGET SCRIPT)
-	if (IS_ABSOLUTE "${SCRIPT}")
-		set(_script_abs "${SCRIPT}")
-	else()
-		get_filename_component(_script_abs "${SCRIPT}" ABSOLUTE)
-	endif()
+	get_source_path(_script_abs "${SCRIPT}")
 	set_property(GLOBAL PROPERTY "${TARGET}_LINK_SCRIPT" "${_script_abs}")
 endmacro()
 
@@ -53,4 +61,31 @@ macro(finish_library TARGET)
 	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${_ldflags}")
 	add_library("${TARGET}" ${_sources})
 	set_property(TARGET "${TARGET}" APPEND PROPERTY LINK_DEPENDS ${_ldscript})
+endmacro()
+
+macro(add_itb TARGET SOURCE)
+	get_source_path(_source "${SOURCE}")
+	get_binary_path(_target "${TARGET}")
+	add_custom_command(
+		OUTPUT "${_target}"
+		COMMAND "${MKIMAGE}"
+			-f "${_source}"
+			-D "-i ${CMAKE_BINARY_DIR} -O dtb"
+			"${_target}"
+		DEPENDS "${_source}"
+	)
+endmacro()
+
+macro(add_dtb TARGET SOURCE)
+	get_source_path(_source "${SOURCE}")
+	get_binary_path(_target "${TARGET}")
+	add_custom_command(
+		OUTPUT "${_target}"
+		COMMAND "${DTC}"
+			-O dtb
+			-o "${_target}"
+			-i "${CMAKE_BINARY_DIR}"
+			"${_source}"
+		DEPENDS "${_source}"
+	)
 endmacro()
